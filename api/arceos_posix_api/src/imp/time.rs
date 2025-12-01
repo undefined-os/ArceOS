1a8a1cd8b33a37cb3e40d4a7fadcbd03dd2afb31
eternalcomet <mydragonos@outlook.com>
2025-12-01 20:58:33 +0800

    feat[arceos_posix_api]: add support for hermit-abi (types, constants, etc.)

--- api/arceos_posix_api/src/imp/time.rs
+++ api/arceos_posix_api/src/imp/time.rs
@@ -21,7 +21,7 @@ impl From<Duration> for ctypes::timespec {
     fn from(d: Duration) -> Self {
         ctypes::timespec {
             tv_sec: d.as_secs() as c_long,
-            tv_nsec: d.subsec_nanos() as c_long,
+            tv_nsec: d.subsec_nanos() as _,
         }
     }
 }
@@ -30,7 +30,7 @@ impl From<Duration> for ctypes::timeval {
     fn from(d: Duration) -> Self {
         ctypes::timeval {
             tv_sec: d.as_secs() as c_long,
-            tv_usec: d.subsec_micros() as c_long,
+            tv_usec: d.subsec_micros() as _,
         }
     }
 }
