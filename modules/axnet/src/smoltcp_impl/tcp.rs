8a114f53aa7afc760145210620237ab5e2560797
eternalcomet <mydragonos@outlook.com>
2025-12-02 17:42:50 +0800

    feat[axnet]: support sys_setsockopt
    Co-authored-by: Shl1015CS <166510542@qq.com>

--- modules/axnet/src/smoltcp_impl/tcp.rs
+++ modules/axnet/src/smoltcp_impl/tcp.rs
@@ -41,6 +41,7 @@ pub struct TcpSocket {
     local_addr: UnsafeCell<IpEndpoint>,
     peer_addr: UnsafeCell<IpEndpoint>,
     nonblock: AtomicBool,
+    reuse_addr: AtomicBool,
 }
 
 unsafe impl Sync for TcpSocket {}
@@ -54,6 +55,7 @@ impl TcpSocket {
             local_addr: UnsafeCell::new(UNSPECIFIED_ENDPOINT),
             peer_addr: UnsafeCell::new(UNSPECIFIED_ENDPOINT),
             nonblock: AtomicBool::new(false),
+            reuse_addr: AtomicBool::new(false),
         }
     }
 
@@ -69,6 +71,7 @@ impl TcpSocket {
             local_addr: UnsafeCell::new(local_addr),
             peer_addr: UnsafeCell::new(peer_addr),
             nonblock: AtomicBool::new(false),
+            reuse_addr: AtomicBool::new(false),
         }
     }
 
@@ -111,6 +114,18 @@ impl TcpSocket {
     #[inline]
     pub fn set_nonblocking(&self, nonblocking: bool) {
         self.nonblock.store(nonblocking, Ordering::Release);
+    }
+
+    /// Returns whether SO_REUSEADDR behavior is enabled.
+    #[inline]
+    pub fn is_reuse_addr(&self) -> bool {
+        self.reuse_addr.load(Ordering::Acquire)
+    }
+
+    /// Enables or disables SO_REUSEADDR behavior.
+    #[inline]
+    pub fn set_reuseaddr(&self, reuse: bool) {
+        self.reuse_addr.store(reuse, Ordering::Release);
     }
 
     /// Connects to the given address and port.
